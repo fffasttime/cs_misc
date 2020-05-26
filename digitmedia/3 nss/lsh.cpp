b8abdd65acd61737da3ad1124a625a5d5791db04
fffasttime <1148716581@qq.com>
2020-05-26 18:32:42 +0800

    add LSH

---
+++ digitmedia/3 nss/lsh.cpp
@@ -0,0 +1,92 @@
+/*
+Author: fffasttime
+Date: 2020/05/26 
+Description: LSH (Locality sensitive hashing), an algorithm 
+for approximate nearest neighbors search problem
+*/
+#include <set>
+#include <map>
+#include <algorithm>
+#include <ctime>
+#include "dataload.hpp"
+using namespace std;
+
+DataVec randvec(){
+	DataVec r;
+	for (int i=0;i<D;i++)
+		r[i]=rand()-RAND_MAX/2;
+	db l=L2(r);
+	for (int i=0;i<D;i++)
+		r[i]/=l;
+	return r;
+}
+
+Data data;
+const int Round=5,Bit=12;
+	
+struct Hash{
+	multimap<int,int> h;
+	vector<DataVec> dir;
+	int gethash(const DataVec &d){
+		int id=0;
+		for (int j=0;j<Bit;j++){
+			db x=dot(dir[j], d);
+			id<<=1;
+			id|=x>0;
+		}
+		return id;
+	}
+	void construct(){
+		dir.resize(Bit);
+		for (int i=0;i<Bit;i++) dir[i]=randvec();
+		for (int i=0;i<N;i++)
+			h.insert({gethash(data[i]),i});
+	}
+	vector<int> search(const DataVec &v){
+		auto range=h.equal_range(gethash(v));
+		vector<int> ret;
+		for (auto it=range.first;it!=range.second;it++)
+			ret.push_back(it->second);
+		return ret;
+	}
+};
+
+int main()
+{
+	Hash h[Round]; // hash table
+	clock_t t0=clock();
+	for (int i=0;i<Round;i++)
+		h[i].construct();
+	cout<<"constructed hash table\n";
+	clock_t t1=clock();
+	
+	int acc=0,mcc=0;
+	//test
+	for (int i=0;i<cN;i++){
+		set<int> nears;
+		for (int j=0;j<Round;j++){
+			auto ret=h[j].search(data[i]);
+			nears.insert(begin(ret),end(ret));
+		}
+		vector<pair<db,int>> near;
+		for (auto v: nears) near.emplace_back(L2(data[i],data[v]),v);
+		//cout<<near.size()<<' ';
+		partial_sort(begin(near),begin(near)+11,end(near));
+		
+		bool matched=1;
+		for (int j=1;j<min(11,(int)near.size());j++){
+			if (near[j].second!=data.real[i][j-1])
+				matched=0;
+			else mcc++;
+		}
+		acc+=matched;
+	}
+	
+	FILE *res=fopen("result.md","a");
+	fprintf(res,"|r=%2d|len=%3d|acc=%5.2f%%|recall=%5.2f%%|%4ld ms|%5ld ms|\n",
+		Round, Bit, (db)acc/cN*100, (db)mcc/cN*10, t1-t0, clock()-t1);
+	fclose(res);
+	
+	return 0;
+}
+
---
+++ digitmedia/3 nss/result.md
@@ -0,0 +1,3 @@
+|r= 5|len= 12|acc=58.00%|recall=71.38%|421 ms|1778 ms|
+|r= 5|len= 12|acc=58.00%|recall=71.38%| 742 ms| 2496 ms|
+|r= 5|len= 12|acc=58.00%|recall=71.38%| 422 ms| 1676 ms|
